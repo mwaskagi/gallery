b5e7087046e788b9ee1a5b8d30537f9b4ef3f7c3
mwaskagi <gituiku.mwangi@gmail.com>
2024-04-13 22:28:45 +0300

    Adding Milestone 2

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://jmwangi:jmwangi123@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://jmwangi:jmwangi123@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://jmwangi:jmwangi123@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    development: 'mongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    test: 'mongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
 }
 module.exports = config;

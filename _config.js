0fea8800cd1437037067fa69dc12d87170400752
mwaskagi <gituiku.mwangi@gmail.com>
2024-04-13 21:42:47 +0300

    Added Milestone 2 header,connected to DB

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mmongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
-    development: 'mmongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
-    test: 'mmongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    production: 'mongodb+srv://jmwangi:jmwangi123@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://jmwangi:jmwangi123@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://jmwangi:jmwangi123@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;

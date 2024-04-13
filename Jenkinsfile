ff34ab64163743eb5647e32f6d9a5304f59d7abb
mwaskagi <gituiku.mwangi@gmail.com>
2024-04-13 19:44:15 +0300

    Adding the MongoDB configs

---
+++ Jenkinsfile
@@ -0,0 +1,22 @@
+
+pipeline {
+  agent any
+
+  stages {
+    stage('Cloning Git') {
+      steps {
+        git branch:'master', url:'git@github.com:mwaskagi/gallery.git'
+      }
+    }
+    stage('Build') {
+       steps {
+         echo "Build"
+       }
+    }
+    stage('Test') {
+      steps {
+         echo "test"
+      }
+    }
+  }
+}
--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mmongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    development: 'mmongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    test: 'mmongodb+srv://jmwangi:jmwangi123@gallery.piugugk.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
 }
 module.exports = config;
